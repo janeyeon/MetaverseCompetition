a8177abadb93035d9dcb8d6cf2d19081c17948c3
janeyeon <khy5630@snu.ac.kr>
2022-08-21 15:12:08 +0900

    :lipstick: add splash view

--- MetaverseCompetition/UI/MainView.swift
+++ MetaverseCompetition/UI/MainView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 extension MainView {
     class ViewModel: ObservableObject {
         @Published var mainViewState: MainViewState
+        @Published var isSplashDone: Bool = false
+        @Published var opacity: Double = 1.0
 
         let container: DIContainer
         private var cancelBag = CancelBag()
@@ -39,6 +41,19 @@ extension MainView {
             container.services.mainViewService.changeMainViewState(to: state)
         }
 
+        /// main view가 켜지자마자 해야하는 일
+        func mainViewAppeared() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    self.opacity = 0.0
+                }
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+                self.isSplashDone = true
+            }
+        }
+
     }
 }
 
@@ -48,21 +63,34 @@ struct MainView: View {
     @StateObject var viewModel: MainView.ViewModel
 
     var body: some View {
-        ZStack{
-            // 항상 보여주는 화면
-            MyARViewControllerRepresentable(viewModel: .init(container: viewModel.container))
+        ZStack {
+            if viewModel.isSplashDone {
+                // main 화면
+                ZStack{
+                    // 항상 보여주는 화면
+                    MyARViewControllerRepresentable(viewModel: .init(container: viewModel.container))
 
-            switch viewModel.mainViewState {
+                    switch viewModel.mainViewState {
 
-            case .addModelState:
-                AddModelStateView(viewModel: .init(container: viewModel.container))
+                    case .addModelState:
+                        AddModelStateView(viewModel: .init(container: viewModel.container))
 
-            case .practiceState:
-                StudyStateView(viewModel: .init(container: viewModel.container))
+                    case .practiceState:
+                        StudyStateView(viewModel: .init(container: viewModel.container))
 
-            case .testState:
-                TestStateView(viewModel: .init(container: viewModel.container))
+                    case .testState:
+                        TestStateView(viewModel: .init(container: viewModel.container))
+                    }
+                }
+            } else {
+                // splash 화면 켜지기
+                SplashView()
+                    .opacity(viewModel.opacity)
             }
         }
+        .onAppear {
+            viewModel.mainViewAppeared()
+        }
+
     }
 }
